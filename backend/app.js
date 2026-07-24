6b7c701bcd8e32c552bc13f80df454a071b005df
sairash00 <sairash7248@gmail.com>
2026-07-24 17:16:23 +0545

    complete backend code before testing

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(morgan("dev"));
 app.get("/", (req, res) => {
     res.json({
         success: true,
-        message: "Server Running 🚀"
+        message: "Server Running "
     });
 });
 
